27f1c6100dc6e24886dbf40b5e85082e4a35b468
Augusto Buccolini <augustobuccolini@Augustos-MacBook-Pro.local>
2024-12-04 13:24:35 -0300

    Categorias fix

--- AppPrincipal/apps.py
+++ AppPrincipal/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
-class AppprincipalConfig(AppConfig):
+class AppPrincipalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AppPrincipal'
+
+    def ready(self):
+        from .models import Categoria
+        if Categoria.objects.count() == 0:
+            Categoria.objects.create(nombre="Instrumentos")
+            Categoria.objects.create(nombre="Accesorios")
